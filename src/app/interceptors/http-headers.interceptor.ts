2ce5b924e4f135bf13a80d6d008c0c1768a9696f
Liam <86805381+LiamO2546@users.noreply.github.com>
2021-11-11 15:45:03 -0800

    finished video game database

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -16,7 +16,7 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
         'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
       },
       setParams: {
-        key: 'e40e743af2c94b0c916a8aa618fb4473',
+        key: 'c89357f365dc444fa6fc1d50bd89462c',
       }
     });
     return next.handle(req);
